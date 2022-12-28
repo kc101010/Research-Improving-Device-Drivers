3995edeb2e0e5c60b7eb13ec56fcbff2b807c462
kc101010 <blueboi.kc@gmail.com>
2022-12-28 18:28:39 +0000

    small progress on writing mouse driver

--- Rust/code/CfromRust/target/debug/build/CfromRust-3a050ffbce56bb11/build_script_build-3a050ffbce56bb11.d
+++
@@ -1,5 +0,0 @@
-/mnt/15f210d7-aa4f-4ea8-a4b3-2bd52aeba8fc/Programming/Uni/Year 4 Hons/Honours-private/Rust Unsafe/CfromRust/target/debug/build/CfromRust-3a050ffbce56bb11/build_script_build-3a050ffbce56bb11: build.rs
-
-/mnt/15f210d7-aa4f-4ea8-a4b3-2bd52aeba8fc/Programming/Uni/Year 4 Hons/Honours-private/Rust Unsafe/CfromRust/target/debug/build/CfromRust-3a050ffbce56bb11/build_script_build-3a050ffbce56bb11.d: build.rs
-
-build.rs:
--- Rust/code/CfromRust/target/debug/deps/CfromRust-87aef901d1846e0e.d
+++
@@ -1,5 +0,0 @@
-/mnt/15f210d7-aa4f-4ea8-a4b3-2bd52aeba8fc/Programming/Uni/Year 4 Hons/Honours-private/Rust Unsafe/CfromRust/target/debug/deps/CfromRust-87aef901d1846e0e: src/main.rs
-
-/mnt/15f210d7-aa4f-4ea8-a4b3-2bd52aeba8fc/Programming/Uni/Year 4 Hons/Honours-private/Rust Unsafe/CfromRust/target/debug/deps/CfromRust-87aef901d1846e0e.d: src/main.rs
-
-src/main.rs:
